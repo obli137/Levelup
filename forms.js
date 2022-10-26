cb317ff2ec193c3eccf7eb3a7a4b5fcfd9610cb7
obli137 <obli137@gmail.com>
2022-10-26 10:41:56 -0300

    First Commit

---
+++ forms.js
@@ -0,0 +1,10 @@
+
+function(){
+
+    var formulario = document.getElementById("formularioDeContacto")
+    elementos = formulario.elements
+    boton = document.getElementById("botonFormularioContacto")
+
+    formulario.addEventListener("submit", validar)
+}
+
